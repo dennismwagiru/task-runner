c76783a3dae7b5d7ef70a09bb918758ee495e04f
Dennis Joel <dennismwagiru@gmail.com>
2021-05-11 16:00:57 +0300

    Project setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,13 @@
 import os
-from flask import Flask, request, jsonify
+
+from flask import Flask
 from flask_pymongo import PyMongo
 
 application = Flask(__name__)
-
+application.config['SECRET'] = os.environ.get("APP_SECRET", '5^jkjkuy4&is8$i900#9%ww2')
 application.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
-
 mongo = PyMongo(application)
 db = mongo.db
-
-@application.route('/')
-def index():
-    return jsonify(
-        status=True,
-        message='Welcome to the Dockerized Flask MongoDB app!'
-    )
-
 
 if __name__ == "__main__":
     ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
---
+++ app/models.py
@@ -0,0 +1,5 @@
+"""
+ @created 11/05/2021 - 3:19 PM
+ @project taskrunner
+ @author dennis joel
+"""
